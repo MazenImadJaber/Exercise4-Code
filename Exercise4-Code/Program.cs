3f8ba8bf9d1943505dcf69a72def64e88904eac1
Mazen Imad Jaber <Mazenimad.jaber@connect.qut.edu.au>
2020-05-03 23:32:05 +1000

    workshop complete heap sort

--- Exercise4-Code/Program.cs
+++ Exercise4-Code/Program.cs
@@ -9,7 +9,7 @@ namespace Exercise4_Code
     class Program
     {
         const int ARRAY_SIZE = 10;
-        const int RANDOM_MAX = 10000;
+        const int RANDOM_MAX = 100;
 
         static void Main(string[] args)
         {
@@ -17,7 +17,7 @@ namespace Exercise4_Code
             int[] nums = new int[ARRAY_SIZE];
             for (int i = 0; i < ARRAY_SIZE; i++)
                 // randomly generate an integer beteen 0 and RAMDOM_MAX
-                nums[i] = rand.Next(100);
+                nums[i] = rand.Next(RANDOM_MAX);
 
 
             Console.Clear();
@@ -37,27 +37,78 @@ namespace Exercise4_Code
                 Console.WriteLine(nums[i]);
             }
 
+
+            // workshop question 
+            int[] example = { 13,57,39,85,70,22,64,48};
+            Console.WriteLine();
+            Console.WriteLine("workshop question before heap sorting:");
+            for (int i = 0; i < example.Length; i++)
+            {
+                Console.WriteLine(example[i]);
+            }
+
+            HeapSort(example);
+
+            Console.WriteLine();
+            Console.WriteLine("after heap sorting:");
+            for (int i = 0; i < example.Length; i++)
+            {
+                Console.WriteLine(example[i]);
+            }
+
             Console.ReadLine();
         }
 
         // convert a complete binary tree into a heap
-        static void HeapBottomUp(int[] data)  //Note: In the algorithm, the array index starts from 1
+        static void HeapBottomUp(int[] H)  //Note: In the algorithm, the array index starts from 1
         {
-            int n = data.Length;
+            int n = H.Length;
             for (int i = (n - 1) / 2; i >= 0; i--)
             {
                 // to be completed
+                int k = i;
+                int v = H[i];
+                bool heap = false;
+                while(!heap && ((2 * k+1) <= (n-1)))
+                {
+                    int j = 2 * k + 1; // left child 
+
+                    if (j < (n-1))
+                    {
+                        if (H[j] < H[j + 1])
+                        {
+                            j=j+1;
+                        }
+                    }
+                    if (v>= H[j])
+                    {
+                        heap = true;
+                    } 
+                    else
+                    {
+                        H[k] = H[j];
+                        k = j;
+
+                    }
+
+                }
+                H[k] = v;
             }
         }
 
         // sort the elements in an array 
         static void HeapSort(int[] data)
         {
-            //Use the HeapBottomUp procedure to convert the array, data, into a heap
+            
+             //Use the HeapBottomUp procedure to convert the array, data, into a heap
             //To be completed
-
+            HeapBottomUp(data);
             //repeatly remove the maximum key from the heap and then rebuild the heap
             //To be completed
+            for(int i = 0; i <=( data.Length-2); i++)
+            {
+                MaxKeyDelete(data, data.Length-i);
+            }
         }
 
         //delete the maximum key and rebuild the heap
@@ -68,9 +119,38 @@ namespace Exercise4_Code
             data[0] = data[size - 1];
             data[size - 1] = temp;
 
-
+            // decrease heap's size by 1
+            int n = size - 1;
+            
+            
             //“Heapify” the complete binary tree.
             // to be completed
+            bool heap = false;
+            int k = 0;
+            int v = data[0];
+            while((!heap) && ((2 * k + 1) <= (n-1)))
+            {
+
+                int j = 2 * k + 1;//left child
+                if (j<(n-1))
+                {
+                    if (data[j] < data[j + 1]) // compare children 
+                    {
+                        j = j + 1; // j is the bigger child of k :3
+                    }
+                }
+                if (v >= data[j])
+                {
+                    heap = true;
+                }
+                else
+                {
+                    data[k] = data[j];
+                    k = j;
+                }
+            }
+            data[k] = v; 
+            
         }
     }
 }
